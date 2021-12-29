77b00bbf6a608a8894ebf1395285a250929754f5
Paulo Pitte <paulopitte@gmail.com>
2021-12-29 15:09:15 -0300

    cleanUp

--- Mkt.Gateway/Mkt.Gateway/Program.cs
+++ Mkt.Gateway/Mkt.Gateway/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 namespace Mkt.Gateway
 {
@@ -17,11 +18,37 @@ namespace Mkt.Gateway
                 // Adiciona através do delegation Action<> a leitura do arquivo de configuração glogal do ocelot
                 .ConfigureAppConfiguration((host, config) =>
                 {
-                    config.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+                     
+
+                    var env = host.HostingEnvironment;
+
+                    config
+                        .AddJsonFile("appsettings.json", false, true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+                        .AddJsonFile("ocelot.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"ocelot.{env.EnvironmentName}.json", true, true);
+
+                    config.AddEnvironmentVariables();
+
+                    if (env.IsDevelopment() || env.IsEnvironment("Local"))
+                    {
+                        var appAssembly = Assembly.Load(new AssemblyName(typeof(Program).Assembly.FullName));
+                        if (appAssembly != null)
+                            config.AddUserSecrets(appAssembly, true);
+                    }
+
+                    if (args != null)
+                        config.AddCommandLine(args);
+
                 })
 
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.AddServerHeader = false;
+                        //serverOptions.ListenAnyIP(6000);
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- Mkt.Gateway/Mkt.Gateway/Startup.cs
+++ Mkt.Gateway/Mkt.Gateway/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -19,23 +18,15 @@ namespace Mkt.Gateway
 
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mkt.Gateway", Version = "v1" });
-            });
-
+        {        
             services.AddOcelot(Configuration);
         }
 
-        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mkt.Gateway v1"));
+                app.UseDeveloperExceptionPage();            
             }
 
             app.UseRouting();
@@ -48,7 +39,7 @@ namespace Mkt.Gateway
                 });
             });
 
-            await app.UseOcelot();
+             app.UseOcelot().Wait();
         }
     }
 }
